6dc460d69ec3c599daa48f0766e69777797d95de
Long_Quoc <longnguyencse@gmail.com>
2016-10-24 14:58:17 +0700

    Initial commit

---
+++ app/src/main/java/cse/bkhcm/pulllayout/PullApplication.java
@@ -0,0 +1,11 @@
+package cse.bkhcm.pulllayout;
+
+import android.app.Application;
+
+/**
+ * Created by nqlong on 09-Sep-16.
+ */
+public class PullApplication extends Application {
+    private static final String TAG = PullApplication.class.getSimpleName();
+
+}
---
+++ app/src/main/java/cse/bkhcm/pulllayout/activity/MainActivity.java
@@ -0,0 +1,13 @@
+package cse.bkhcm.pulllayout.activity;
+
+/**
+ * Created by nqlong on 24-Oct-16.
+ */
+
+public class MainActivity extends AbstractActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+}
